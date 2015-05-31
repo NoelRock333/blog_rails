b008c8d83ad48cf4d399e6490f962154112fbe22
Noel Escobedo <noel_ska15@hotmail.com>
2015-05-31 00:43:10 -0500

    Se creó la vista de nuevo articulo, se agregaron algunos estilos y se creó la acción de crear articulo

--- app/controllers/articulos_controller.rb
+++ app/controllers/articulos_controller.rb
@@ -6,6 +6,18 @@ class ArticulosController < ApplicationController
 
 	#GET /articulos/:id
 	def show
-		@articulo = Articulo.find(params[:id])
+		@articulo = Articulo.find(params[:id]) #Obtiene el parametro id que viene en la url
+	end
+
+	#GET /articulos/new
+	def new
+		@articulo = Articulo.new #Crea una instancia del modelo Articulo
+	end
+
+	#POST /articulos/create
+	def create
+		@articulo = Articulo.new(titulo: params[:articulo][:titulo], cuerpo: params[:articulo][:cuerpo])
+		@articulo.save
+		redirect_to @articulo
 	end
 end
