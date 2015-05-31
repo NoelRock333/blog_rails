eae0ad9c9751b7a339d8c6e48f1a487f20c49345
Noel Escobedo <noel_ska15@hotmail.com>
2015-05-31 17:15:11 -0500

    Agregamos metodos para crear un nuevo articulo con todo y validaciones

--- app/controllers/articulos_controller.rb
+++ app/controllers/articulos_controller.rb
@@ -14,10 +14,15 @@ class ArticulosController < ApplicationController
 		@articulo = Articulo.new #Crea una instancia del modelo Articulo
 	end
 
-	#POST /articulos/create
+	#POST /articulos
 	def create
 		@articulo = Articulo.new(titulo: params[:articulo][:titulo], cuerpo: params[:articulo][:cuerpo])
-		@articulo.save
-		redirect_to @articulo
+		#Tambien podriamos usar el metodo @articulo.valid? o @articulo.invalid?
+		#Si no se puede guardar el articulo, se vuelve a mostrar la vista new
+		if @articulo.save
+			redirect_to @articulo #Nos redirige
+		else
+			render :new #No nos mueve de acción, simplemente le dice que use la vista :new
+		end
 	end
 end
--- app/models/articulo.rb
+++ app/models/articulo.rb
@@ -1,2 +1,5 @@
 class Articulo < ActiveRecord::Base
+	validates :titulo, presence: true, uniqueness: true #Valida que el elemento no venga vacio y que no se repita
+	validates :cuerpo, presence: true, length: { minimum: 20 } #Valida que el campo cuerpo no esté vacio y que contenga un minimo de 20 caracteres
+	#validates :username, format: { with: /regex/ } #Validacion con expresión regular
 end
