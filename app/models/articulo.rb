66bbdcbcb9adb25c0a679f9f3dc87a3c4fed6058
noelrock333 <noel_ska15@hotmail.com>
2015-06-03 13:08:27 -0500

    Eliminada linea que actualizaba el valor del contador del articulo cuando era nil

--- app/models/articulo.rb
+++ app/models/articulo.rb
@@ -6,16 +6,13 @@ class Articulo < ActiveRecord::Base
 	before_save :set_visits_count #tambien existen otros metodos como "before_save", "before_validation", "after_create", "afeter_save", "after_save :privada, on: :create" entre otros
 
 	def update_visits_count
-		#Aqui no estamos guardando nada, pero si el contador de visitas es igual a 'nil' entonces dispara el 'before_save' 
-		#que a su vez ejecuta el 'set_visits_count' para tomar el valor actual, pero si viene 'nil' entonces toma un 0 
-		#y luego ya incrementa en 1 y guarda la modificación
-		self.save if self.contador_visitas.nil?
+		#self.save if self.contador_visitas.nil?  #Una vez ya teniendo todos los registros con valor diferente de nil en la base de datos, ahora si podemos borrar esta linea
 		self.update(contador_visitas: self.contador_visitas + 1) #Le decimos que actualice el contador de visitas con el valor del contador de visitas mas uno
 	end
 
 	private
 
 	def set_visits_count
-		self.contador_visitas ||= 0 
+		self.contador_visitas ||= 0 #Inicia el campo contador_visitas en 0 en dado caso de que no traiga nada
 	end
 end
